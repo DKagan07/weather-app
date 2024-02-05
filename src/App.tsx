266d37a9c8b04f61d8b8c6fe66b6455a16063f26
David Kagan <davidckagan@sbcglobal.net>
2024-02-04 19:58:04 -0600

    Update FE
    Updated frontend to be set up for server work to call API

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import type { Component } from "solid-js"
 import Home from "./Home/Home"
 
-async function sendBasicRequest() {
-    const response = await fetch("/api")
-    const result = await response.json()
-    console.log("result: ", result)
-    return result
-}
+// This is an example of a good query to the "/" endpoint in my go server
+// async function sendBasicRequest() {
+// 	const response = await fetch("/api")
+// 	const result = await response.json()
+// 	console.log("result: ", result)
+// 	return result
+// }
 
 const App: Component = () => {
-    return (
-        <>
-            <Home />
-            <button onClick={sendBasicRequest}>Test</button>
-        </>
-    )
+	return <Home />
 }
 
 export default App
--- src/Home/queryzipcode.ts
+++ src/Home/queryzipcode.ts
@@ -1,10 +1,10 @@
 export async function queryZipCode(query: string) {
-    // Query is empty, don't return anything
-    if (query.trim() === "") return []
+	// Query is empty, don't return anything
+	if (query.trim() === "") return []
 
-    // now have to talk to backend
-    // I imagine since the server is open at this endpoint, we have to do
-    // something like this:
-    const response = await fetch("localhost:3001/<zipcode>")
-    return response.json()
+	// now have to talk to backend
+	// I imagine since the server is open at this endpoint, we have to do
+	// something like this:
+	const response = await fetch(`/api/{query}`)
+	return response.json()
 }
