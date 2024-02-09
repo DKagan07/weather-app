3ad6f4e2a782a7985a920d9267be83437b287089
David Kagan <davidckagan@sbcglobal.net>
2024-02-08 20:59:59 -0600

    Fixed compile of app and server
    App wouldn't compile, fixed that because it was still referencing a css page.
    
    For the server, the template in the docs used a middleware to attach the url
    Param to the context. I had trouble fetching that and type-casting it, so I
    removed the middleware and retreived the param in the main handler.

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -9,8 +8,6 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
-
-type ctxKey string
 
 func main() {
 	r := chi.NewRouter()
@@ -28,44 +25,21 @@ func main() {
 		})
 
 		r.Route("/{zipCode}", func(r chi.Router) {
-			r.Use(ZipCodeCtx)
 			r.Get("/", ReadWeather)
 		})
 	})
 
-	// r.Get("/api", func(w http.ResponseWriter, r *http.Request) {
-	// 	res, err := json.Marshal("Welcome, Linda!")
-	// 	if err != nil {
-	// 		w.Write([]byte("error"))
-	// 	}
-	// 	w.Write(res)
-	// })
-	//    r.Route("/api/{zipCode}", func(r chi.Router {
-	//        r.Get("/", zipCodeHandler.ReadZipCode)
-	//    }))
-
+	// Serve's up, man
 	if err := http.ListenAndServe(":3001", r); err != nil {
 		panic("cannot ListenAndServe")
 	}
 }
 
-func ZipCodeCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		zipCode := chi.URLParam(r, "zipCode")
-		ctx := context.WithValue(r.Context(), ctxKey("zipcode"), zipCode)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
 func ReadWeather(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	// the .(*string) is what the type of what zipcode is
-	// Not too sure if this has to be the type of the return of the API call
-	// (I think it does)
-	zipcode, ok := ctx.Value(ctxKey("zipcode")).(*string)
-	if !ok {
-		http.Error(w, http.StatusText(422), 422)
-	}
+	// getting the zip code from the URL, as a string
+	zipcode := chi.URLParam(r, "zipCode")
+
 	// need to do something with `zipcode`
+	// probably an api call with that zip code....need to find what API to use
 	fmt.Println(zipcode)
 }
