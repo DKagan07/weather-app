225593d568fcd30110b680cfb390d97885035306
David Kagan <davidckagan@sbcglobal.net>
2024-02-11 21:08:12 -0600

    Got data showing?

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,59 +1,54 @@
-import { createResource, createSignal } from "solid-js"
+import { createEffect, createResource, createSignal, For, Show } from "solid-js"
 import { WeatherOutput } from "./queryzipcode"
 
 export const queryZipCode = async (query: string) => {
-    if (query.length === 0) return
-    const response = await fetch("/api/" + query)
-    const res = (await response.json()) as WeatherOutput
-    return res
+	if (query.length === 0) return
+	const response = await fetch("/api/" + query)
+	const res = (await response.json()) as WeatherOutput
+	return res
 }
 
 export default function Home() {
-    const [inputSignal, setInputSignal] = createSignal("")
-    const [querySignal, setQuerySignal] = createSignal("")
-    const [weatherResource] = createResource(querySignal, queryZipCode)
+	const [inputSignal, setInputSignal] = createSignal("")
+	const [querySignal, setQuerySignal] = createSignal("")
+	const [weatherResource] = createResource(querySignal, queryZipCode)
 
-    return (
-        <>
-            <form>
-                <h2> Put in Your US Zip Code to Get the Weather</h2>
-                <div>
-                    <label for="zipcode">Search here </label>
-                    <input
-                        id="zipcode"
-                        value={inputSignal()}
-                        placeholder="Input Zip Code"
-                        type="text"
-                        inputmode="numeric"
-                        onInput={(e) => {
-                            setInputSignal(e.currentTarget.value?.trim())
-                        }}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    onClick={(e) => {
-                        e.preventDefault()
-                        setQuerySignal(inputSignal())
-                    }}
-                >
-                    Look up
-                </button>
-            </form>
+	createEffect(() => console.log(weatherResource(), weatherResource.loading))
 
-            {console.log(weatherResource(), weatherResource.loading)}
+	return (
+		<>
+			<form>
+				<h2> Put in Your US Zip Code to Get the Weather</h2>
+				<div>
+					<label for="zipcode">Search here </label>
+					<input
+						id="zipcode"
+						value={inputSignal()}
+						placeholder="Input Zip Code"
+						type="text"
+						inputmode="numeric"
+						onInput={(e) => {
+							setInputSignal(e.currentTarget.value?.trim())
+						}}
+					/>
+				</div>
+				<button
+					type="submit"
+					onClick={(e) => {
+						e.preventDefault()
+						setQuerySignal(inputSignal())
+					}}
+				>
+					Look up
+				</button>
+			</form>
 
-            {/* <Show when={weatherResource.error}> ERROR MESSAGE </Show>
-    
-            <Show when={!weatherResource.loading && !weatherResource.error} fallback={<>Loading...</>}>
-                <h1>{weatherResource()?.Location?.Name} {weatherResource()?.Current?.Temp_c}</h1>
-    
-                <ul>
-                    <For each={weatherResource()?.Forecast?.Forecastday}>
-                        {(day, index) => <li data-key={index()}>{day?.Date_epoch}</li>}
-                    </For>
-                </ul>
-                </Show> */}
-        </>
-    )
+			<Show when={weatherResource.error}> ERROR MESSAGE </Show>
+
+			<Show when={!weatherResource.loading} fallback={<>Loading...</>}>
+				{/* This shows all the data, now I think I need to parse it in an element and branch off?*/}
+				{JSON.stringify(weatherResource())}
+			</Show>
+		</>
+	)
 }
