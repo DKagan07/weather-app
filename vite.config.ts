b45c3b5808135629e1d29543576ba2ad128770e7
David Kagan <davidckagan@sbcglobal.net>
2024-02-19 14:45:40 -0600

    Formatting and adding Forecast stuff
    Still TODO: make the divs appear horizontally

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
+import { writeSignal } from "solid-js/types/reactive/signal"
 import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-    plugins: [
-        /* 
+	plugins: [
+		/* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
-        // devtools(),
-        solidPlugin(),
-    ],
-    server: {
-        port: 3000,
-        proxy: {
-            "/api": {
-                target: "http://localhost:3001/",
-                changeOrigin: true,
-                secure: false,
-            },
-        },
-    },
-    build: {
-        target: "esnext",
-    },
+		// devtools(),
+		solidPlugin(),
+	],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:3001/",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+	build: {
+		target: "esnext",
+	},
 })
