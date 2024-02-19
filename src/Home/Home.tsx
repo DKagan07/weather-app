ccf3e11a01e94000c0f478564e4329084c892581
David Kagan <davidckagan@sbcglobal.net>
2024-02-19 14:07:35 -0600

    renamed file, fleshed out more info

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,48 +1,48 @@
 import { createEffect, createResource, createSignal, Show } from "solid-js"
 import { queryZipCode } from "./queryzipcode"
-import WeatherInfo from "./weatherInfo"
+import WeatherInfo from "./WeatherInfo"
 
 export default function Home() {
-	const [inputSignal, setInputSignal] = createSignal("")
-	const [querySignal, setQuerySignal] = createSignal("")
-	const [weatherResource] = createResource(querySignal, queryZipCode)
+    const [inputSignal, setInputSignal] = createSignal("")
+    const [querySignal, setQuerySignal] = createSignal("")
+    const [weatherResource] = createResource(querySignal, queryZipCode)
 
-	createEffect(() => console.log(weatherResource(), weatherResource.loading))
+    createEffect(() => console.log(weatherResource(), weatherResource.loading))
 
-	return (
-		<>
-			<form>
-				<h2> Put in Your US Zip Code to Get the Weather</h2>
-				<div>
-					<label for="zipcode">Search here </label>
-					<input
-						id="zipcode"
-						value={inputSignal()}
-						placeholder="Input Zip Code"
-						type="text"
-						inputmode="numeric"
-						onInput={(e) => {
-							setInputSignal(e.currentTarget.value?.trim())
-						}}
-					/>
-				</div>
-				<button
-					type="submit"
-					onClick={(e) => {
-						e.preventDefault()
-						setQuerySignal(inputSignal())
-					}}
-				>
-					Look up
-				</button>
-			</form>
+    return (
+        <>
+            <form>
+                <h2> Put in Your US Zip Code to Get the Weather</h2>
+                <div>
+                    <label for="zipcode">Search here </label>
+                    <input
+                        id="zipcode"
+                        value={inputSignal()}
+                        placeholder="Input Zip Code"
+                        type="text"
+                        inputmode="numeric"
+                        onInput={(e) => {
+                            setInputSignal(e.currentTarget.value?.trim())
+                        }}
+                    />
+                </div>
+                <button
+                    type="submit"
+                    onClick={(e) => {
+                        e.preventDefault()
+                        setQuerySignal(inputSignal())
+                    }}
+                >
+                    Look up
+                </button>
+            </form>
 
-			<Show when={weatherResource.error}> ERROR MESSAGE </Show>
+            <Show when={weatherResource.error}> ERROR MESSAGE </Show>
 
-			<Show when={!weatherResource.loading} fallback={<>Loading...</>}>
-				{/* This shows all the data, now I think I need to parse it in an element and branch off?*/}
-				<WeatherInfo wi={weatherResource()} />
-			</Show>
-		</>
-	)
+            <Show when={!weatherResource.loading} fallback={<>Loading...</>}>
+                {/* This shows all the data, now I think I need to parse it in an element and branch off?*/}
+                <WeatherInfo wi={weatherResource()} />
+            </Show>
+        </>
+    )
 }
--- src/Home/queryzipcode.ts
+++ src/Home/queryzipcode.ts
@@ -1,8 +1,8 @@
 export const queryZipCode = async (query: string) => {
-	if (query.length === 0) return
-	const response = await fetch("/api/" + query)
-	const res = (await response.json()) as WeatherOutput
-	return res
+    if (query.length === 0) return
+    const response = await fetch("/api/" + query)
+    const res = (await response.json()) as WeatherOutput
+    return res
 }
 
 //**************************************************************
@@ -11,147 +11,147 @@ export const queryZipCode = async (query: string) => {
 //
 //**************************************************************
 export type WeatherOutput = {
-	location: Location
-	current: Current
-	forecast: Forecast
+    location: location
+    current: current
+    forecast: forecast
 }
 
-type Location = {
-	name: string
-	region: string
-	country: string
-	lat: number
-	lon: number
-	tz_id: string
-	localtime_epoch: number
-	localtime: string
+type location = {
+    name: string
+    region: string
+    country: string
+    lat: number
+    lon: number
+    tz_id: string
+    localtime_epoch: number
+    localtime: string
 }
 
-type Current = {
-	last_updated_epoch: number
-	last_updated: string
-	temp_c: number
-	temp_f: number
-	is_day: number
-	condition: condition
-	wind_mph: number
-	wind_kph: number
-	wind_degree: number
-	wind_dir: string
-	pressure_mb: number
-	pressure_in: number
-	precip_mm: number
-	precip0_in: number
-	humidity: number
-	cloud: number
-	feelslike_c: number
-	feelslike_f: number
-	vis_km: number
-	vis_miles: number
-	uv: number
-	gust_mph: number
-	gust_kph: number
-	air_quality: airquality
+type current = {
+    last_updated_epoch: number
+    last_updated: string
+    temp_c: number
+    temp_f: number
+    is_day: number
+    condition: condition
+    wind_mph: number
+    wind_kph: number
+    wind_degree: number
+    wind_dir: string
+    pressure_mb: number
+    pressure_in: number
+    precip_mm: number
+    precip_in: number
+    humidity: number
+    cloud: number
+    feelslike_c: number
+    feelslike_f: number
+    vis_km: number
+    vis_miles: number
+    uv: number
+    gust_mph: number
+    gust_kph: number
+    air_quality: airquality
 }
 
 type airquality = {
-	co: number
-	no2: number
-	o3: number
-	so2: number
-	pm2_5: number
-	pm10: number
-	us_epa_index: number
-	gb_defra_index: number
+    co: number
+    no2: number
+    o3: number
+    so2: number
+    pm2_5: number
+    pm10: number
+    us_epa_index: number
+    gb_defra_index: number
 }
 
 type condition = {
-	text: string
-	icon: string
-	code: number
+    text: string
+    icon: string
+    code: number
 }
 
-type Forecast = {
-	forecastday: forecastday[]
+type forecast = {
+    forecastday: forecastday[]
 }
 
 type forecastday = {
-	date: string
-	date_epoch: number
-	day: day
-	astro: astro
-	hour: hour[]
+    date: string
+    date_epoch: number
+    day: Day
+    astro: astro
+    hour: hour[]
 }
 
 type astro = {
-	sunrise: string
-	sunset: string
-	moonrise: string
-	moon_phase: string
-	moon_illumination: number
-	is_moon_up: number
-	is_sun_up: number
+    sunrise: string
+    sunset: string
+    moonrise: string
+    moon_phase: string
+    moon_illumination: number
+    is_moon_up: number
+    is_sun_up: number
 }
 
-type day = {
-	maxtemp_c: number
-	maxtemp_f: number
-	mnumberemp_c: number
-	mnumberemp_f: number
-	avgtemp_c: number
-	avgtemp_f: number
-	maxwind_mph: number
-	maxwind_kph: number
-	totalprecip_mm: number
-	totalprecip_in: number
-	totalsnow_cm: number
-	avgvis_km: number
-	avgvis_miles: number
-	avghumidity: number
-	daily_will_it_rain: number
-	daily_chance_of_rain: number
-	daily_will_it_snow: number
-	daily_chance_of_snow: number
-	condition: condition
-	uv: number
-	air_quality: airquality
+export type Day = {
+    maxtemp_c: number
+    maxtemp_f: number
+    mintemp_c: number
+    mintemp_f: number
+    avgtemp_c: number
+    avgtemp_f: number
+    maxwind_mph: number
+    maxwind_kph: number
+    totalprecip_mm: number
+    totalprecip_in: number
+    totalsnow_cm: number
+    avgvis_km: number
+    avgvis_miles: number
+    avghumidity: number
+    daily_will_it_rain: number
+    daily_chance_of_rain: number
+    daily_will_it_snow: number
+    daily_chance_of_snow: number
+    condition: condition
+    uv: number
+    air_quality: airquality
 }
 
 type hour = {
-	time_epoch: number
-	time: string
-	temp_c: number
-	temp_f: number
-	is_day: number
-	condition: condition
-	wind_mph: number
-	wind_kph: number
-	wind_degree: number
-	wind_dir: string
-	pressure_mb: number
-	pressure_in: number
-	precip_mm: number
-	precip0_in: number
-	humidity: number
-	cloud: number
-	feelslike_c: number
-	feelslike_f: number
-	windchill_c: number
-	windchill_f: number
-	heatindex_c: number
-	heatindex_f: number
-	dewponumber_c: number
-	dewponumber_f: number
-	will_it_rain: number
-	chance_of_rain: number
-	will_it_snow: number
-	chance_of_snow: number
-	vis_km: number
-	vis_miles: number
-	gust_mph: number
-	gust_kph: number
-	uv: number
-	air_quality: airquality
-	short_rad: number
-	diff_rad: number
+    time_epoch: number
+    time: string
+    temp_c: number
+    temp_f: number
+    is_day: number
+    condition: condition
+    wind_mph: number
+    wind_kph: number
+    wind_degree: number
+    wind_dir: string
+    pressure_mb: number
+    pressure_in: number
+    precip_mm: number
+    precip0_in: number
+    humidity: number
+    cloud: number
+    feelslike_c: number
+    feelslike_f: number
+    windchill_c: number
+    windchill_f: number
+    heatindex_c: number
+    heatindex_f: number
+    dewponumber_c: number
+    dewponumber_f: number
+    will_it_rain: number
+    chance_of_rain: number
+    will_it_snow: number
+    chance_of_snow: number
+    vis_km: number
+    vis_miles: number
+    gust_mph: number
+    gust_kph: number
+    uv: number
+    air_quality: airquality
+    short_rad: number
+    diff_rad: number
 }
